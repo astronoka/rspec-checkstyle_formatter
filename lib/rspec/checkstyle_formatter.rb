e719b78c2745126d69f4c5db37dcdf3e1716d40e
astronoka <astronoka@users.noreply.github.com>
2020-10-10 20:13:22 +0900

    Abracadabra
    bundle gem rspec-checkstyle_formatter -t

---
+++ lib/rspec/checkstyle_formatter.rb
@@ -0,0 +1,8 @@
+require "rspec/checkstyle_formatter/version"
+
+module Rspec
+  module CheckstyleFormatter
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rspec/checkstyle_formatter/version.rb
@@ -0,0 +1,5 @@
+module Rspec
+  module CheckstyleFormatter
+    VERSION = "0.1.0"
+  end
+end
